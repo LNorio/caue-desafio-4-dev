f65cc9913da1d639e9e49a6df9495d86608de44f
caueaquino <cauenogueira@alunos.utfpr.edu.br>
2018-10-08 04:09:12 -0300

    update telas/add push firebase

---
+++ js/actionframe.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+    $("#divavaliacao").click(function(){
+    $("#mid").slideToggle(170);
+    $("#cadavaliacao").slideToggle(170);
+    });
+    });
+
+$(document).ready(function(){
+    $("#divcliente").click(function(){
+    $("#mid").slideToggle(170);
+    $("#cadcliente").slideToggle(170);
+    });
+    });
+
+$(document).ready(function(){
+    $("#cancelarcad").click(function(){
+    $("#cadcliente").slideToggle(170);
+    $("#mid").slideToggle(170);
+    });
+    });
+
+$(document).ready(function(){
+    $("#cancelarava").click(function(){
+    $("#ravaliacao").slideToggle(170);
+    $("#avali").slideToggle(170);
+    });
+    });
+
+$(document).ready(function(){
+    $("#respavaliacao").click(function(){
+    $("#avali").slideToggle(170);
+    $("#ravaliacao").slideToggle(170);
+    });
+    });
+
+$(document).ready(function(){
+    $("#cancelarca").click(function(){
+    $("#cadavaliacao").slideToggle(170);
+    $("#mid").slideToggle(170);
+    });
+    });
+
+$(document).ready(function(){
+    $("#divcadastros").click(function(){
+    $("#init").slideToggle(170);
+    $("#mid").slideToggle(170);
+    });
+    });
+
+$(document).ready(function(){
+    $("#voltarmid").click(function(){
+    $("#mid").slideToggle(170);
+    $("#init").slideToggle(170);
+    });
+    });
+
+$(document).ready(function(){
+    $("#voltaravaliacao").click(function(){
+    $("#avali").slideToggle(170);
+    $("#init").slideToggle(170);
+    });
+    });
+
+$(document).ready(function(){
+    $("#divavaliacoes").click(function(){
+    $("#init").slideToggle(170);
+    $("#avali").slideToggle(170);
+    });
+    });
--- js/main.js
+++ js/main.js
@@ -1,21 +1,40 @@
-// Javascript Document
+function cadcliente(){
+    var clientInput=document.getElementById('cnomecliente');
+    var contatoInput=document.getElementById('cnomecontato');
 
-var admin=require("firebase-admin");
-var db = admin.database();
-var ref = db.ref("server/saving-data/fireblog");
-
-function cadastrarCliente(){
-    if(document.getElementById("cnomecliente").value=="" || document.getElementById("cnomecontato").value==""){
+    if(clientInput.value=="" || contatoInput.value==""){
         alert("Preencha o(s) Campo(s) para concluir o cadastro!");
-    }else if(false){
+    }else if(verifCliente(clientInput)){
         alert("Nome de Cliente ja existe!");
     }else{
-
+        var data=Date();
+        criaCliente(clientInput.value, contatoInput.value, data);
+        alert("Cadastro Concluido!");
     }
     limparCampos();
 }
 
+function verifCliente(cl){
+    return true;
+}
+
+function criaCliente(cliente, contato, data){
+    var dat = {
+        cliente: cliente,
+        contato: contato,
+        cadData: data,
+        categoria: "nenhum",
+        avData: ""
+    };
+
+    return firebase.database().ref().child('clientes').push(dat);
+}
+
 function limparCampos(){
-    document.getElementById("cnomecliente").value="";
-    document.getElementById("cnomecontato").value="";
+    document.getElementById('cnomecliente').value="";
+    document.getElementById('cnomecontato').value="";
+}
+
+function cadavaliacao(){
+    alert('erro');
 }
