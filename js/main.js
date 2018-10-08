0a7229f09d0c3ff8ba9cc9d7e81644a2a0071111
caueaquino <cauenogueira@alunos.utfpr.edu.br>
2018-10-07 22:07:02 -0300

    update files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 // Javascript Document
+
+var admin=require("firebase-admin");
+var db = admin.database();
+var ref = db.ref("server/saving-data/fireblog");
+
+function cadastrarCliente(){
+    if(document.getElementById("cnomecliente").value=="" || document.getElementById("cnomecontato").value==""){
+        alert("Preencha o(s) Campo(s) para concluir o cadastro!");
+    }else if(false){
+        alert("Nome de Cliente ja existe!");
+    }else{
+
+    }
+    limparCampos();
+}
+
+function limparCampos(){
+    document.getElementById("cnomecliente").value="";
+    document.getElementById("cnomecontato").value="";
+}
